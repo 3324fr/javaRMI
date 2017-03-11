1625a009bc8d883256fa191783905160566ee797
lam <ba.samson.lam@gmail.com>
2017-03-11 13:39:36 -0500

    merge

---
+++ src/ca/polymtl/inf4410/tp2/server/AbstractServer.java
@@ -0,0 +1,26 @@
+package ca.polymtl.inf4410.tp2.server;
+
+import java.rmi.ConnectException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import ca.polymtl.inf4410.tp2.shared.ServerInterface;
+
+public class AbstractServer implements ServerInterface {
+
+	@Override
+	public int execute(int a, int b) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public static void main(String[] args) {
+	}
+
+	public AbstractServer() {
+	}
+
+	private void run() {
+	}
+}
