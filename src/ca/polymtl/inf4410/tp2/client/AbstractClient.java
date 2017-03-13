96d34583b91d1d40e3e68f5ef518c9f27a6874c4
Fred <3324fr@gmail.com>
2017-03-13 13:32:40 -0400

    remove thread.sleep() in client

--- src/ca/polymtl/inf4410/tp2/client/AbstractClient.java
+++ src/ca/polymtl/inf4410/tp2/client/AbstractClient.java
@@ -29,14 +29,14 @@ abstract public class AbstractClient {
 
 	private final static String DISTANTHOSTNAMEFILE = "host.csv";
 	private final static int PORT = 5002;
-	
+
 	protected static final int TIMEOUT = 9000;
 	protected static HashMap<String,ServerStub> distantServerStubs = null;
 	protected static ArrayList<TaskRunnable> Tasks = null;
 
 	private Iterator<Entry<String, ServerStub>> iterator;
-	
-	
+
+
 	public AbstractClient(ArrayList<ItemOperation> listOperation, ArrayList<String> hosts ) {
 		super();
 
@@ -49,8 +49,8 @@ abstract public class AbstractClient {
 			String[] parts = host.split(" ");
 			ServerInterface stub = loadServerStub(parts[0]);
 			if(stub == null){
-                            System.err.print("Stub is null. EXITING");
-                            continue;
+				System.err.print("Stub is null. EXITING");
+				continue;
 			}
 			int qi = Integer.valueOf(parts[1]);
 			distantServerStubs.put(host,new ServerStub(stub,parts[0],qi));
@@ -64,23 +64,23 @@ abstract public class AbstractClient {
 			int chunk = serverStub.qi*2;
 			int j = (i + chunk);
 			if(j > size)
-                            j = size;
-                        System.out.println("i : " + i);
-                        System.out.println("j : " + j);
+				j = size;
+			System.out.println("i : " + i);
+			System.out.println("j : " + j);
 			ArrayList<ItemOperation> test = new ArrayList<ItemOperation>(listOperation.subList(i,j));
-                        printList(test);
+			printList(test);
 			TaskRunnable task = new TaskRunnable(test,serverStub);
 			Tasks.add(task);
 			i = j;                    
 		}
-		
+
 
 	}
-	
+
 	private void printList(ArrayList<ItemOperation> list){
-                for(ItemOperation op : list){
-                    System.out.println("In printing test : "  + op.value);
-                }
+		for(ItemOperation op : list){
+			System.out.println("In printing test : "  + op.value);
+		}
 	}
 
 	private  ServerStub getDistantServerStub(){
@@ -95,7 +95,7 @@ abstract public class AbstractClient {
 	}
 	protected static  ServerStub getDistantServerStub(int qi){
 		for(Map.Entry<String, ServerStub> entry : distantServerStubs.entrySet()){
-                        System.out.println("entry.getValue().qi " + entry.getValue().qi + " qi " + qi);
+			System.out.println("entry.getValue().qi " + entry.getValue().qi + " qi " + qi);
 			if(entry.getValue().qi >= qi)
 				return entry.getValue();
 		}
@@ -150,114 +150,108 @@ abstract public class AbstractClient {
 		}
 
 		private void start2(ArrayList<ItemOperation> listOperation){
-                        ++counter;
-                        if(counter > MAX_THREAD_SIZE){
-                            //System.out.println("");
-                            try{
-                                t.sleep(10);
-                            //System.exit(-2);
-                            }
-                            catch (Exception e){
-                                e.printStackTrace();
-                            }
-                        }
-                        System.out.println("In Start2 fired-----");
+			++counter;
+			if(counter > MAX_THREAD_SIZE){
+				System.out.println("Erreur: Impossible de trouver un serveur pour la t�che.");
+				System.exit(-2);
+
+			}
+			System.out.println("In Start2 fired-----");
 			int listOperationSize = listOperation.size();  
 			this.serverStub = getDistantServerStub();
-			if(listOperationSize < 3){
+			if(this.serverStub.qi*5 < listOperationSize){
 				this.run();
 			}
 			else{
-				
-                                    int halfsize =listOperationSize/2;
-                                    //System.out.println("Size " + halfsize);
-                                    ServerStub serverStub1 = getDistantServerStub();
-                                    ServerStub serverStub2 = getDistantServerStub();
-                                    
-                                    ArrayList<ItemOperation> list1 = new ArrayList<ItemOperation>(listOperation.subList(0, halfsize));
-                                    
-                                    
-                                    ArrayList<ItemOperation> list2 = new ArrayList<ItemOperation>(listOperation.subList(halfsize, listOperationSize));
-                                    
-                                    
-                                    TaskRunnable task1 = new TaskRunnable(list1,serverStub1);
-                                    TaskRunnable task2 = new TaskRunnable(list2,serverStub2);
-                                    
-                                    
-                                    task1.start();
-                                    task2.start();
-                                    
-                                    try {
-                                            task1.t.join(TIMEOUT);
-                                            calculValue(task1.getReturnValue());
-                                            //this.returnValue = (this.returnValue+task1.getReturnValue())% 4000;
-                                    } catch (InterruptedException e) {
-                                            task1.isValidResult = false;					
-                                    }
-                                    if(!task1.isValidResult)
-                                            start2(task1.listOperation);
-                                    try {
-                                            task2.t.join(TIMEOUT);
-                                            calculValue(task2.getReturnValue());
-                                            //this.returnValue = (this.returnValue+task2.getReturnValue())% 4000;
-                                    } catch (InterruptedException e) {
-                                            task2.isValidResult = false;					
-                                    }
-                                
-                                    if(!task2.isValidResult)
-                                            start2(task2.listOperation);
-                                            
 
-                                            
-                                }
-			
-		}
-		
-		public void run() {
-					
-		scheduler.scheduleAtFixedRate(new  Runnable() {
-                                        @Override
-                                        public void run() {
-                                                if(!checkServerBreakdown(serverStub.getHostname())){
-                                                        System.out.println("There is a breakdown. Restarting op");
-                                                        //start2(listOperation);
-                                                }	
-                                                System.out.println("There is a breakdown. dsadasd op");
-                                        }
-                                },1000, 1000,TimeUnit.MILLISECONDS);
+				int halfsize =listOperationSize/2;
+				ServerStub serverStub1 = getDistantServerStub();
+				ServerStub serverStub2 = getDistantServerStub();
 
-             
-			if( this.serverStub != null){
-                            try {
-                                    System.out.println("In serverStub "+serverStub.hostname+". Execute fired-----");
-                                    //returnValue =  this.serverStub.getStub().execute(5);
-                                    //System.out.println("In serverStub "+serverStub.hostname+". Run fired-----" + returnValue);
-                                    
-                                    //returnValue =  this.serverStub.getStub().receiveOperation(listOperation);
-                                    //System.out.println(Arrays.toString(listOperation.toArray()));
-                                    int reponse =  this.serverStub.getStub().receiveOperation(listOperation.toArray(new ItemOperation[listOperation.size()]));
-                                    System.out.println("In serverStub "+serverStub.hostname+". Run result -----" + reponse);
-                                    calculValue(reponse);
-                                    
-                                    isValidResult = true;
-                            } catch (RemoteException e) {
-                                    isValidResult = false;
-                                    System.out.println("Erreur: "  + e.getMessage());	
-                            } finally {
-                                
-                                if(!isValidResult){
-                                    isValidResult = true;
-                                    start2(listOperation);
-                                }
-                            }
-                    
+				ArrayList<ItemOperation> list1 = new ArrayList<ItemOperation>(listOperation.subList(0, halfsize));
+
+
+				ArrayList<ItemOperation> list2 = new ArrayList<ItemOperation>(listOperation.subList(halfsize, listOperationSize));
+
+
+				TaskRunnable task1 = new TaskRunnable(list1,serverStub1);
+				TaskRunnable task2 = new TaskRunnable(list2,serverStub2);
+
+
+				task1.start();
+				task2.start();
+
+				try {
+					task1.t.join(TIMEOUT);
+					calculValue(task1.getReturnValue());
+					//this.returnValue = (this.returnValue+task1.getReturnValue())% 4000;
+				} catch (InterruptedException e) {
+					task1.isValidResult = false;					
+				}
+				if(!task1.isValidResult)
+					start2(task1.listOperation);
+				try {
+					task2.t.join(TIMEOUT);
+					calculValue(task2.getReturnValue());
+					//this.returnValue = (this.returnValue+task2.getReturnValue())% 4000;
+				} catch (InterruptedException e) {
+					task2.isValidResult = false;					
+				}
+
+				if(!task2.isValidResult)
+					start2(task2.listOperation);
+
+
+
 			}
-			
-			
-				
+
+		}
+
+		public void run() {
+
+			scheduler.scheduleAtFixedRate(new  Runnable() {
+				@Override
+				public void run() {
+					if(!checkServerBreakdown(serverStub.getHostname())){
+						System.out.println("There is a breakdown. Restarting op");
+						//start2(listOperation);
+					}	
+					System.out.println("There is a breakdown. dsadasd op");
+				}
+			},1000, 1000,TimeUnit.MILLISECONDS);
+
+
+			if( this.serverStub != null){
+				try {
+					System.out.println("In serverStub "+serverStub.hostname+". Execute fired-----");
+					//returnValue =  this.serverStub.getStub().execute(5);
+					//System.out.println("In serverStub "+serverStub.hostname+". Run fired-----" + returnValue);
+
+					//returnValue =  this.serverStub.getStub().receiveOperation(listOperation);
+					//System.out.println(Arrays.toString(listOperation.toArray()));
+					int reponse =  this.serverStub.getStub().receiveOperation(listOperation.toArray(new ItemOperation[listOperation.size()]));
+					System.out.println("In serverStub "+serverStub.hostname+". Run result -----" + reponse);
+					calculValue(reponse);
+
+					isValidResult = true;
+				} catch (RemoteException e) {
+					isValidResult = false;
+					System.out.println("Erreur: "  + e.getMessage());	
+				} finally {
+
+					if(!isValidResult){
+						isValidResult = true;
+						start2(listOperation);
+					}
+				}
+
+			}
+
+
+
 		}
 		public void start(){
-			
+
 			if (t == null) {
 				t = new Thread (this);
 				System.out.println("In Thread t start fired-----");
@@ -265,10 +259,10 @@ abstract public class AbstractClient {
 			}
 
 		}
-		
+
 		private void calculValue(int value){
-                    System.out.println("in calcul: " + value + " fdsfsdf " + this.returnValue);
-                    this.returnValue = (this.returnValue+value)% 4000;
+			System.out.println("in calcul: " + value + " fdsfsdf " + this.returnValue);
+			this.returnValue = (this.returnValue+value)% 4000;
 		}
 	}
 
@@ -280,7 +274,7 @@ abstract public class AbstractClient {
 		System.out.println("Temps écoulé appel RMI distant: "
 				+ (end - start) + " ns");
 		System.out.println("Résultat appel RMI distant: " + result);	
-                
+
 	}
 
 	private ServerInterface loadServerStub(String hostname) {
@@ -330,24 +324,24 @@ abstract public class AbstractClient {
 	}
 	public static ArrayList<ItemOperation> readOperationFile(String filename) throws IOException {
 		ArrayList<ItemOperation> list = new ArrayList<>();
-		
+
 		//System.out.println("pwd : " + System.getProperty("user.dir"));
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		try {
 
 			String line = br.readLine();
 			while (line != null) {   
-                                String[] parts = line.split(" ");
+				String[] parts = line.split(" ");
 				switch (parts[0]) {
 				case "prime":  list.add(new Prime(parts[1]));
 				break;
 				case "pell":  list.add(new Pell(parts[1]));
 				break;
 				default:
-				break;
+					break;
 				}
 				line = br.readLine();
-				
+
 				//System.out.println(list.get(list.size()-1).value);
 			}
 		} 
